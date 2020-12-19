a3618efd547934f5eaf8749202f7723fba398265
Herman Westerman <58404234+namwe@users.noreply.github.com>
2020-12-19 22:47:51 +0100

    Move implementation to class

--- Stickman.py
+++ Stickman.py
@@ -2,14 +2,13 @@ import random, string
 
 class Stickman:
     lines = None
-    dict = {"-1", -1}
+    words_dict = None
     rmdStr = None
 
-    def __init__(self):
+    def __init__(self, word):
         self.lines = 0
-        for i in range(25):
-            temp = chr(i)
-            dict.update("a", 5)
+        for i in range(97, 123):
+            self.words_dict = {chr(i): word}
 
     def draw(self):
         # asrad
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from random import *
 
 if __name__ == '__main__':
     root = tk.Tk()
@@ -9,6 +10,17 @@ if __name__ == '__main__':
     with open("words.txt") as f:
         my_list = f.readlines()
 
+    s = ""
+    while len(s) <= 6:
+        x = randint(1, 360000)
+        s = my_list[x]
+
+    for i in range(97, 123):
+        temp = chr(i)
+        #print(temp)
+
+    print(x)
+    print(s)
 
 
     #root.mainloop()
